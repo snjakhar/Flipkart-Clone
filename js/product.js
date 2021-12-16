6cc422d0dcea4e0fa47ee359ef2f7e4f33b6e518
Akhilg-02 <guptaakhil0206@gmail.com>
2021-12-16 17:08:39 +0530

    data --

--- js/product.js
+++ js/product.js
@@ -1,53 +1,178 @@
-let key = "3b1upy8OYY1QZCEFx6LVDJgT";
 
-let url = `https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))?apiKey=${key}&sort=sku.asc&show=sku,name,thumbnailImage,image,description,regularPrice,customerReviewCount,features.feature&format=json`;
+var data = [
+    {
+              name: "Lenovo IdeaPad Gaming 3 Core i5 10th Gen - (8 GB/512 GB SSD/Windows 10 Home/4 GB Graphics/NVIDIA GeFor...",
+              rating:"4.3",
+              d1: "Intel Core i5 Processor (10th Gen)",
+              d2:"8 GB DDR4 RAM",
+              d3:"64 bit Windows 10 Oprating System",
+              d4:"512 GB SSD",
+              d5:"39.62 cm (15.6 inch) DIsplay",
+              d6:"1 Year Warranty + 1 Year ADP + 1 Year Premium care",
+              price:58990,
+              img:"https://rukminim1.flixcart.com/image/312/312/kp2y2kw0/computer/3/q/k/lenovo-original-imag3eht25nykhjn.jpeg?q=70",
+          },
+          {
+              name: "Lenovo Legion 5 Ryzen 5 Hexa Core 4600H - (8 GB/1 TB HDD/256 GB SSD/Windows 10 Home/4 GB Graphics/NVID...",
+              rating:"4.5",
+              d1: "AMD Ryzen 5 Hexa Processor ",
+              d2:"8 GB DDR4 RAM",
+              d3:"64 bit Windows 10 Oprating System",
+              d4:"1 TB SSD",
+              d5:"39.62 cm (15.6 inch) DIsplay",
+              d6:"1 Year Onsite Warranty ",
+              price:67690 ,
+              img:"https://rukminim1.flixcart.com/image/312/312/kk76wsw0/computer/h/j/o/lenovo-original-imafzhsyzryckmew.jpeg?q=70",
+          },
+          {
+              name: "ASUS TUF Gaming F15 Core i5 10th Gen - (8 GB/512 GB SSD/Windows 10 Home/4 GB Graphics/NVIDIA GeForce G.",
+              rating:"4.4",
+              d1: "Intel Core i5 Processor (10th Gen)",
+              d2:"8 GB DDR4 RAM",
+              d3:"64 bit Windows 10 Oprating System",
+              d4:"512 GB SSD",
+              d5:"39.62 cm (15.6 inch) DIsplay",
+              d6:"1 Year Warranty + 1 Year ADP + 1 Year Premium care",
+              price: 49990,
+              img:"https://rukminim1.flixcart.com/image/312/312/ku4ezrk0/computer/2/z/l/fx506hcb-hn228t-gaming-laptop-asus-original-imag7bhenhg7nqvh.jpeg?q=70",
+          },
+          {
+              name: "Bleach Cream",
+              rating:"4.4",
+              d1: "Intel Core i5 Processor (10th Gen)",
+              d2:"8 GB DDR4 RAM",
+              d3:"64 bit Windows 10 Oprating System",
+              d4:"512 GB SSD",
+              d5:"39.62 cm (15.6 inch) DIsplay",
+              d6:"2 Year Warranty Team Gaming ",
+              price: 55990,
+              img:"https://rukminim1.flixcart.com/image/312/312/kp1imq80/computer/u/b/z/na-gaming-laptop-acer-original-imag3d63hk3h8nm2.jpeg?q=70",
+          },
+          {
+              name: "MSI GF63 Thin Core i7 10th Gen - (8 GB/1 TB HDD/256 GB SSD/Windows 10 Home/4 GB Graphics/NVIDIA GeForc...",
+              rating:"4.3",
+              d1: "Intel Core i7 Processor (10th Gen)",
+              d2:"16 GB DDR4 RAM",
+              d3:"64 bit Windows 10 Oprating System",
+              d4:"1 TB SSD",
+              d5:"39.62 cm (15.6 inch) DIsplay",
+              d6:"2 Year Warranty Team Gaming ",
+              price: 64990,
+              img:"https://rukminim1.flixcart.com/image/312/312/ko7idu80/computer/x/l/i/gf63-thin-10scxr-1616in-notebook-msi-original-imag2q55h6w2avfr.jpeg?q=70",
+          },
+          {
+              name: "ASUS VivoBook 15 (2021) Core i3 11th Gen - (4 GB/256 GB SSD/Windows 10 Home) X515EA-EJ302TS Thin and Light Laptop",
+              rating:"4.3",
+              d1: "Intel Core i5 Processor (10th Gen)",
+              d2:"16 GB DDR4 RAM",
+              d3:"64 bit Windows 10 Oprating System",
+              d4:"1 TB SSD",
+              d5:"39.62 cm (15.6 inch) DIsplay",
+              d6:"Microsoft Office Home & Student 2019",
+              price: 64990,
+              img:"https://rukminim1.flixcart.com/image/312/312/ksyz8280/computer/f/9/9/x515ea-ej302ts-thin-and-light-laptop-asus-original-imag6fc9kxzc6rrh.jpeg?q=70",
+          },
+          {
+              name: "HP Pavilion Gaming Ryzen 5 Hexa Core 4600H - (8 GB/1 TB HDD/Windows 10 Home/4 GB Graphics/NVIDIA GeForce GTX 1650) 15-ec1021AX Gaming Laptop",
+              rating:"4.4",
+              d1: "Intel Core i5 Processor (10th Gen)",
+              d2:"8 GB DDR4 RAM",
+              d3:"64 bit Windows 10 Oprating System",
+              d4:"512 GB SSD",
+              d5:"39.62 cm (15.6 inch) DIsplay",
+              d6:"2 Year Warranty Team Gaming ",
+              price: 55990,
+              img:"https://rukminim1.flixcart.com/image/312/312/kbqu4cw0/computer/q/x/r/hp-original-imaftyzachgrav8f.jpeg?q=70",
+          },
+          {
+              name: "Lenovo Legion 5 Ryzen 5 Hexa Core 4600H - (8 GB/1 TB HDD/256 GB SSD/Windows 10 Home/4 GB Graphics/NVID...",
+              rating:"4.5",
+              d1: "AMD Ryzen 5 Hexa Processor ",
+              d2:"8 GB DDR4 RAM",
+              d3:"64 bit Windows 10 Oprating System",
+              d4:"1 TB SSD",
+              d5:"39.62 cm (15.6 inch) DIsplay",
+              d6:"1 Year Onsite Warranty ",
+              price:67690 ,
+              img:"https://rukminim1.flixcart.com/image/312/312/kk76wsw0/computer/h/j/o/lenovo-original-imafzhsyzryckmew.jpeg?q=70",
+          },
 
-async function getData(){
+          
+    ]
 
-    let res = await fetch(url);
-    let data = await res.json();
 
-    console.log(data.products);
 
-    showData(data.products);
-}
+
+
+
+
+
 //-----------------------------------------------------
 
 let right = document.getElementById("right");
-
-
-function showData(info){
-    info.forEach(function(item){
+ 
+function showData(){
+  data.forEach(function(item){
 
     var main = document.createElement("div");
-    main.setAttribute("id","main")
+    main.setAttribute("id","main");
+    
 
-    var img = document.createElement("img");
-    img.setAttribute("class","prodImage")
-    img.src = item.image;
-
-
-    var price =  document.createElement("h2");
-    price.setAttribute("class","priceTag")
-    price.innerHTML = "₹" + Math.round(item.regularPrice) * 76;
-
+    var image = document.createElement("img");
+    image.setAttribute("class","prodImage")
+    image.setAttribute("src", item.img);
 
     var title =  document.createElement("h4","div");
     title.setAttribute("class","prodData")
     title.innerHTML = item.name;
 
-    var f = document.createElement("p")
-    for(let i=1; i< 5; i++){
-       var x = item.features[i].feature;
-       if()
-      
+    var rate = document.createElement("div");
+     rate.setAttribute("class","rate");
+     rate.innerHTML = item.rating + "★";
 
-    } console.log(x,"here")
-     title.append(x);
-   
+    var text1 =  document.createElement("div");
+    text1.innerHTML = `<h5 class="review">Rating & Review</h5>`
+
+    var d1 = document.createElement("li");
+    d1.setAttribute("class","dots");
+    d1.innerHTML = item.d1;
+
+    var d2 = document.createElement("li");
+    d2.setAttribute("class","dots");
+    d2.innerHTML = item.d2
+
+    var d3 = document.createElement("li");
+    d3.setAttribute("class","dots");
+    d3.innerHTML = item.d3
+
+    var d4 = document.createElement("li");
+    d4.setAttribute("class","dots");
+    d4.innerHTML = item.d4
+
+    var d5 = document.createElement("li");
+    d5.setAttribute("class","dots");
+    d5.innerHTML = item.d5
+
+    var d6 = document.createElement("li");
+    d6.setAttribute("class","dots");
+    d6.innerHTML = item.d6
+
+    var price =  document.createElement("h2");
+    price.setAttribute("class","priceTag")
+    price.innerHTML = "₹" + item.price
+ 
+     var img1 =document.createElement("img");
+     img1.setAttribute("class","assure");
+     img1.src = "https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/fa_62673a.png";
 
     
-    main.append(img,title,price);
+ 
+
+     price.append(img1);
+    
+     title.append(rate,text1,d1,d2,d3,d4,d5,d6);
+
+    main.append(image,title,price);
 
     right.append(main);
 
@@ -55,4 +180,21 @@ function showData(info){
     })
 }
 
-getData()
+showData()
+
+
+
+
+// let key = "3b1upy8OYY1QZCEFx6LVDJgT";
+
+// let url = `https://api.bestbuy.com/v1/products((categoryPath.id=abcat0502000))?apiKey=${key}&sort=sku.asc&show=sku,name,thumbnailImage,image,description,regularPrice,customerReviewCount,features.feature&format=json`;
+
+// async function getData(){
+
+//     let res = await fetch(url);
+//     let data = await res.json();
+
+//     console.log(data.products);
+
+//     showData(data.products);
+// }
